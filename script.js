b11f6ccfd7e4b214fff5a15c3b41ffedcf4cf214
perezmarcella <marcella.perez@yahoo.com.br>
2023-10-18 13:34:23 -0300

    código atualizado e responsividade

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-const rock = document.querySelector(".pedra")
-const paper = document.querySelector(".papel")
-const scissors = document.querySelector(".tesoura")
-const lizard = document.querySelector(".lagarto")
-const spock = document.querySelector(".spock")
+const result = document.querySelector(".result")
+const yourScore = document.querySelector(".your-score")
+const machineNumber = document.querySelector(".machine-score")
 
-function playHuman (humanChoice) {
-    console.log(humanChoice)
+let humanScore = 0
+let machineScore = 0
+
+const playHuman = (humanChoice) => {
+   
+    playTheGame(humanChoice, playMachine())
 
 }
 
+const playMachine = () => {
+    const choices = ["rock", "paper", "scissors", "lizard", "spock"]
+    const randomResult = Math.floor(Math.random() * 5)
 
+    return choices[randomResult]
 
-rock.addEventListener("click", playHuman("rock"))
-/*paper.addEventListener("click", playHuman)
-scissors.addEventListener("click", playHuman)
-lizard.addEventListener("click", playHuman)
-spock.addEventListener("click", playHuman)*/
+}
+
+const playTheGame = (human, machine) => {
+
+    console.log("Humano" + human + "Máquina" + machine)
+       
+    if(human === machine) {
+        result.innerHTML = "Deu empate!"
+    } else if (
+        human === "scissors" && machine === "paper" ||
+        human === "scissors" && machine === "lizard" ||
+        human === "paper" && machine === "rock" ||
+        human === "paper" && machine === "spock" ||
+        human === "rock" && machine === "lizard" ||
+        human === "rock" && machine === "scissors" ||
+        human === "lizard" && machine === "spock" ||
+        human === "lizard" && machine === "paper" ||
+        human === "spock" && machine === "scissors" ||
+        human === "spock" && machine === "rock"
+    ) {
+        humanScore++
+        result.innerHTML = "Você venceu!"
+        yourScore.innerHTML = humanScore
+    } else {
+        machineScore++
+        result.innerHTML = "Você perdeu!"
+        machineNumber.innerHTML = machineScore
+    }
+    
+}
